cbbf5876f56e39501420e77c8d2c68c56d71654c
Yasir Erkam <yasir.erkam17@gmail.com>
2020-09-17 16:04:10 +0300

    commit

--- VadeliHesap/Form1.Designer.cs
+++ VadeliHesap/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnTahminEt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(241, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VADELİ HESAP TAHMİNİ";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(104, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(542, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Müşterinin vadeli para yatırma işlemine abone olup olmayacağını tahmin ettirmek i" +
+    "çin ";
+            // 
+            // BtnTahminEt
+            // 
+            this.BtnTahminEt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnTahminEt.AutoSize = true;
+            this.BtnTahminEt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTahminEt.Location = new System.Drawing.Point(320, 197);
+            this.BtnTahminEt.Name = "BtnTahminEt";
+            this.BtnTahminEt.Size = new System.Drawing.Size(111, 39);
+            this.BtnTahminEt.TabIndex = 3;
+            this.BtnTahminEt.Text = "Tahmin Et";
+            this.BtnTahminEt.UseVisualStyleBackColor = true;
+            this.BtnTahminEt.Click += new System.EventHandler(this.BtnTahminEt_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(226, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(302, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "\"Tahmin Et\" butonuna basıp soruları yanıtlayın.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(738, 301);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnTahminEt);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vadeli Hesap Tahmini";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnTahminEt;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- VadeliHesap/Form1.cs
+++ VadeliHesap/Form1.cs
@@ -16,5 +16,173 @@ namespace VadeliHesap
         {
             InitializeComponent();
         }
+
+        private void BtnTahminEt_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Müşterinin son iletişim süresi (duraiton) 1531.5 sn'den küçük ya da eşit mi?", "Müşteri Bilgileri", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            {
+                if (DialogResult.Yes == MessageBox.Show("Müşterinin son iletişim süresi (duraiton) 772.5 sn'den küçük ya da eşit mi?", "Müşteri Bilgileri", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                {
+                    if (DialogResult.Yes == MessageBox.Show("Müşterinin son iletişim süresi (duraiton) 645 sn'den küçük ya da eşit mi?", "Müşteri Bilgileri", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                    {
+                        MessageBox.Show("TAHMİN SONUCU : Müşteri vadeli para yatırma işlemine abone OLMAYACAK.", "SONUÇ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        if (DialogResult.Yes == MessageBox.Show("Önceki bir kampanyadan müşteriyle en son iletişim kurulduktan sonra geçen gün sayısı (pDays) 122'den küçük ya da eşit mi?", "Müşteri Bilgileri", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                        {
+                            MessageBox.Show("TAHMİN SONUCU : Müşteri vadeli para yatırma işlemine abone OLMAYACAK.", "SONUÇ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            MessageBox.Show("TAHMİN SONUCU : Müşteri vadeli para yatırma işlemine abone OLACAK.", "SONUÇ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                else
+                {
+                    if (DialogResult.Yes == MessageBox.Show("Müşterinin ortalama yıllık bakiyesi (balance) -909.5'dan küçük ya da eşit mi?", "Müşteri Bilgileri", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                    {
+                        MessageBox.Show("TAHMİN SONUCU : Müşteri vadeli para yatırma işlemine abone OLMAYACAK.", "SONUÇ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        if (DialogResult.Yes == MessageBox.Show("Müşterinin son iletişim süresi (duraiton) 1518.5 sn'den küçük ya da eşit mi?", "Müşteri Bilgileri", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                        {
+                            MessageBox.Show("TAHMİN SONUCU : Müşteri vadeli para yatırma işlemine abone OLACAK.", "SONUÇ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("TAHMİN SONUCU : Müşteri vadeli para yatırma işlemine abone OLMAYACAK.", "SONUÇ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (DialogResult.Yes == MessageBox.Show("Son iletişime geçilme dahil bu kampanya sırasında müşteriyle iletişime geçilme sayısı (campaign) 2.5'dan küçük ya da eşit mi?", "Müşteri Bilgileri", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                {
+                    if (DialogResult.Yes == MessageBox.Show("Müşterinin son iletişim süresi (duraiton) 1730.5 sn'den küçük ya da eşit mi?", "Müşteri Bilgileri", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                    {
+                        MessageBox.Show("TAHMİN SONUCU : Müşteri vadeli para yatırma işlemine abone OLACAK.", "SONUÇ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        if (DialogResult.Yes == MessageBox.Show("Müşterinin yaşı 43.5'dan küçük ya da eşit mi?", "Müşteri Bilgileri", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                        {
+                            MessageBox.Show("TAHMİN SONUCU : Müşteri vadeli para yatırma işlemine abone OLMAYACAK.", "SONUÇ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            MessageBox.Show("TAHMİN SONUCU : Müşteri vadeli para yatırma işlemine abone OLACAK.", "SONUÇ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("TAHMİN SONUCU : Müşteri vadeli para yatırma işlemine abone OLACAK.");
+                }
+            }
+        }
+
+        public bool TahminEt(Musteri musteri)
+        {
+            if (musteri.duration <= 1531.5f)
+            {
+                if (musteri.duration <= 772.5f)
+                {
+                    if (musteri.duration <= 645)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (musteri.previousDays <= 122.5f)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else
+                {
+                    if (musteri.balance <= 909.5f)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (musteri.duration <= 1518.5f)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (musteri.campaign <= 2.5f)
+                {
+                    if (musteri.duration <= 1730.5f)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (musteri.age <= 43.5f)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public struct Musteri
+        {
+            private enum Job
+            {
+                admin, unknown, unemployed, management, housemaid, entrepreneur, student, blueCollar, selfEmployed, retired, technician, services
+            }
+
+            private enum Marital
+            {
+                married, divorced, single
+            }
+
+            private enum Education
+            {
+                secondary, primary, tertiary
+            }
+
+            private enum Month
+            {
+                Ocak, Subat, Mart, Nisan, Mayis, Haziran, Temmuz, Agustos, Eylul, Ekim, Kasim, Aralik
+            }
+
+            private enum PreviousOutcome
+            {
+                admin, unknown, unemployed, management, housemaid, entrepreneur, student, blueCollar, selfEmployed, retired, technician, services
+            }
+
+            public int age, balance, day, duration, campaign, previousDays, previous;
+
+            public bool deflt, housing, loan;
+        }
     }
 }
